7760b83cacf7944daf89ddd4d6c9770b69cf5915
krospigliosig <krospigliosig@unsa.edu.pe>
2025-07-10 04:48:47 -0500

    optimización de detección de tabler

--- opencv/patron_ajedrez.cpp
+++ opencv/patron_ajedrez.cpp
@@ -1,32 +1,32 @@
 #include <opencv2/opencv.hpp>
+#include <glm/glm.hpp>
 #include <iostream>
 
-cv::Point3f chess_center_point(cv::Mat frame, cv::Size patternSize, std::vector<cv::Point2f> corners) {
+std::pair<bool, cv::Point3i> chess_center_point(cv::Mat& frame, cv::Size patternSize, std::vector<cv::Point2f>& corners) {
     cv::Mat gray;
-
     cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
 
-    cv::Point3f center(0.f, 0.f, 0.f);
+    cv::Point3f center(0, 0, 0);
 
-    bool found = cv::findChessboardCorners(
-        gray, patternSize, corners,
-        cv::CALIB_CB_ADAPTIVE_THRESH | cv::CALIB_CB_NORMALIZE_IMAGE
-    );
+    bool found = cv::findChessboardCornersSB(gray, patternSize, corners);
 
     if (found) {
         cv::drawChessboardCorners(frame, patternSize, corners, found);
-        
-        for (const auto& corner : corners) {
-            center.x += corner.x;
-            center.y += corner.y;
-        }
 
-        center.x /= (float)corners.size();
-        center.y /= (float)corners.size();
+        if (!corners.empty()) {
+            for (const auto& corner : corners) {
+                center.x += corner.x;
+                center.y += corner.y;
+            }
+
+            center.x /= (float)corners.size();
+            center.y /= (float)corners.size();
+        }
     }
 
-    return center;
+    return std::make_pair(found, center);
 }
+
 
 int main() {
     cv::VideoCapture cap(0);
@@ -44,6 +44,7 @@ int main() {
     cv::Mat gray;
 
     std::vector<cv::Point2f> corners;
+    corners.reserve(static_cast<std::vector<cv::Point2f, std::allocator<cv::Point2f>>::size_type>(chessboard_cols) * chessboard_rows);
 
     bool running = true;
 
@@ -51,12 +52,12 @@ int main() {
         cap >> frame;
         if (frame.empty()) break;
 
-        auto center = chess_center_point(frame, patternSize, corners);
+        auto chess_detect = chess_center_point(frame, patternSize, corners);
 
-        cv::putText(frame, "Ajedrez detectado", { center.x, center.y }, cv::FONT_HERSHEY_SIMPLEX, 1.0, { 0, 255, 0 }, 2);
+        if (chess_detect.first) cv::putText(frame, "Ajedrez detectado", { chess_detect.second.x, chess_detect.second.y }, cv::FONT_HERSHEY_SIMPLEX, 1.0, { 0, 255, 0 }, 2);
 
         cv::imshow("Detección de ajedrez", frame);
-        if (cv::waitKey(33) == 27) running = false; // 27 -> Esc; 33 -> maxFPS = 30 FPS
+        if (cv::waitKey(1) == 27) running = false; // 27 -> Esc; 33 -> maxFPS = 30 FPS
     }
 
     cap.release();
